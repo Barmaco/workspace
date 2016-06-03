401089cf9875d3dc14a71fedc69f97d2c1679902
barmaco <992077171@qq.com>
2016-06-03 10:11:32 +0800

    first commit

---
+++ helloredis/helloredis.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : helloredis.cpp
+// Author      : barmaco
+// Version     :
+// Copyright   : qq:992077171
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!!!");
+	return EXIT_SUCCESS;
+}
